fbba82d93cc0eb1009fb69da3020113094610983
Arlindo Junior <arlindo.junior@sga.pucminas.br>
2025-02-22 09:27:21 -0300

    feat: criando repositories

---
+++ src/main/java/com/restaurante/restaurante/model/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.restaurante.restaurante.model.repositories;
+
+import com.restaurante.restaurante.model.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
---
+++ src/main/java/com/restaurante/restaurante/model/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.restaurante.restaurante.model.repositories;
+
+import com.restaurante.restaurante.model.entities.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
